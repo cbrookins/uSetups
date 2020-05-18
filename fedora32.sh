57bc86c147a4f734ab2fea5a3c441b55d4cf8c1f
chris / brookins <chris@brookins.info>
2020-05-18 13:57:23 -0400

    Update fedora32.sh

--- fedora32.sh
+++ fedora32.sh
@@ -11,7 +11,7 @@ sudo dnf -y update
 clear
 
 echo "Adding RPM Fusion repos"
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 clear
 
