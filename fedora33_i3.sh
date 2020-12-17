467231be56e41eaaaeca09f1c8fe9d6b46d68900
chris / brookins <chris@brookins.info>
2020-12-17 14:46:57 -0500

    Update fedora33_i3.sh

--- fedora33_i3.sh
+++ fedora33_i3.sh
@@ -4,8 +4,34 @@
 ##
 ## Update system and install packages
 ##
-echo "Updating system and installing wanted packages"
-sudo dnf update && sudo dnf install -y i3 xinit xfce4-terminal feh nano git mutt weechat ranger firefox xorg-x11-server-Xorg
+echo "Updating system"
+sudo dnf update 
+clear
+
+echo "Adding RPM Fusion repos"
+sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+clear
+
+echo "Adding Microsoft repos"
+## Register Visual Studio Code repo
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+## Register the Microsoft RedHat repository
+curl https://packages.microsoft.com/config/fedora/33/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
+curl https://packages.microsoft.com/config/rhel/7/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
+clear
+
+echo "Installing software..."
+sudo dnf -y check-update
+sudo dnf install -y i3 xinit xfce4-terminal feh nano git mutt weechat ranger firefox xorg-x11-server-Xorg kernel-devel broadcom-wl compat-openssl10 seahorse gtkhash ffmpeg ffmpeg-libs vlc fuse-exfat exfat-utils peek powershell dkms
+clear
+
+## Enable BCM43228 WiFi Card
+echo "Prepping WiFi Kernel Module.."
+git clone https://github.com/antoineco/broadcom-wl
+dkms add ./broadcom-wl
 clear
 
 ##
@@ -31,3 +57,7 @@ clear
 ##
 echo "exec /usr/bin/i3" >> ~/.xinitrc
 echo "# sh ~/.fehbg &" >> ~/.xinitrc
+
+echo "Complete.  Rebooting in 10s..."
+sleep 10
+reboot
